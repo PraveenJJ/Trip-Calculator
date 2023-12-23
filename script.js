1c343dd2c216c0093db8e3934603ff0d6b304624
PraveenJJ <praveen_jj123@yahoo.com>
2023-12-23 16:18:29 +0530

    introduction of tabs 1.0

--- script.js
+++ script.js
@@ -87,3 +87,62 @@ document.addEventListener('DOMContentLoaded', function () {
     odometerOne.render(); // Render the odometer
     odometerTwo.render(); // Render the odometer
 });
+
+
+// Add this to your script.js file
+function openTab(tabId) {
+    // Get all elements with class "tab-content" and hide them
+    var tabContents = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active');
+    }
+
+    // Get all elements with class "tab-btn" and remove the "active" class
+    var tabButtons = document.getElementsByClassName('tab-btn');
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active');
+    }
+
+    // Show the selected tab content
+    document.getElementById(tabId).classList.add('active');
+
+    // Add the "active" class to the clicked tab button
+    event.currentTarget.classList.add('active');
+}
+
+function validateAndCalculateFuelCost() {
+    // Remove previous highlighting
+    var inputs = document.querySelectorAll('.required-field');
+    inputs.forEach(function (input) {
+        input.classList.remove('required-field');
+    });
+
+    // Input validation logic
+    var distanceToTravel = parseFloat(document.getElementById('distanceToTravelF').value);
+    var fuelPrice = parseFloat(document.getElementById('fuelPriceF').value);
+    var mileage = parseFloat(document.getElementById('mileageF').value);
+
+    // Highlight empty fields and return if any are empty
+    if (!distanceToTravel) {
+        document.getElementById("distanceToTravel").classList.add('required-field');
+    }
+    if (!fuelPrice) {
+        document.getElementById("fuelPrice").classList.add('required-field');
+    }
+    if (!mileage) {
+        document.getElementById("mileage").classList.add('required-field');
+    }
+
+    // Check if any required field is empty and return without executing further
+    if (!distanceToTravel || !fuelPrice || !mileage) {
+        return;
+    }
+
+    // Perform calculation logic here...
+    // Example: document.getElementById("dynamicContentBox").value = calculateResult;
+    // Calculate drivable kms
+    var fuelCost = Math.round((distanceToTravel / mileage) * fuelPrice);
+
+    // Display the result
+    document.getElementById('fuelCostF').innerHTML = fuelCost;
+}
